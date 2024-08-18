e84cfd4d7079f800c2a9ccc525fc8f78411a32de
HtetAungHlaing24899 <htetaunghlaing240899@gmail.com>
2024-08-18 23:04:45 +0700

    adding python scripts, environment variables set up and importing db_details in app.py

---
+++ scripts/config.py
@@ -0,0 +1,25 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+DB_DETAILS = {
+    'dev': {
+        'SOURCE_DB': {
+        'DB_TYPE': 'mysql',
+        'DB_HOST': 'mysql-db',
+        'DB_PORT': 3306,
+        'DB_USER': os.getenv('MYSQL_USER'),
+        'DB_PASSWORD': os.getenv('MYSQL_PASSWORD'),
+        'DB_NAME': os.getenv('MYSQL_DATABASE')
+        },
+        'TARGET_DB': {
+            'DB_TYPE': 'postgresql',
+            'DB_HOST': 'postgres-db',
+            'DB_PORT': 5432,
+            'DB_USER': os.getenv('POSTGRES_USER'),
+            'DB_PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+            'DB_NAME': os.getenv('POSTGRES_DB')
+        },
+    }
+}
