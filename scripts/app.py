a10f4243b568d7160530f6bd7f4df2871f0ddbd8
HtetAungHlaing24899 <htetaunghlaing240899@gmail.com>
2024-08-19 23:24:35 +0700

    adding a function to get table list from csv

--- scripts/app.py
+++ scripts/app.py
@@ -1,10 +1,16 @@
 import sys
 from config import DB_DETAILS
+from util import get_tables
 
 def main():
+    #getting arguments from command line and get db_details based on the argument
     env = sys.argv[1]
     db_details = DB_DETAILS[env]
 
+    #getting table list from the csv
+    tables = get_tables('../table_list.txt')
+    for table in tables['table_name']:
+        print(table)
 
 
 if __name__ == '__main__':
