d0ae1030e4d2bf7de26e754cab964d1a128534b9
HtetAungHlaing24899 <htetaunghlaing240899@gmail.com>
2024-08-21 14:23:49 +0700

    updating main function to use custom functions to load db details and read data

--- scripts/app.py
+++ scripts/app.py
@@ -1,18 +1,19 @@
 import sys
 from config import DB_DETAILS
 from util import get_tables
+from util import load_db_details
+from read import read_table
 
 def main():
     #getting arguments from command line and get db_details based on the argument
     env = sys.argv[1]
-    db_details = DB_DETAILS[env]
+    db_details = load_db_details(env)
 
-    print(db_details)
-
-    #getting table list from the csv
-    tables = get_tables('../table_list.txt')
-    for table in tables['table_name']:
-        print(table)
+    #reading data from db
+    table_name = 'departments'
+    data, columns = read_table(db_details, table_name)
+    for column in columns:
+        print(column)
 
 
 if __name__ == '__main__':
