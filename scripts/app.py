3597c0c7a28222be43035885fbcf4f33df71e9b1
HtetAungHlaing24899 <htetaunghlaing240899@gmail.com>
2024-09-08 14:19:50 +0700

    adding insert_data function to insert data into postgres

--- scripts/app.py
+++ scripts/app.py
@@ -3,6 +3,7 @@ from config import DB_DETAILS
 from util import get_tables
 from util import load_db_details
 from read import read_table
+from write import build_insert_query
 
 def main():
     #getting arguments from command line and get db_details based on the argument
@@ -12,10 +13,10 @@ def main():
     #reading data from db
     tables = get_tables('../table_list.txt')
     for table_name in tables['table_name']:
+        print(f'reading data for {table_name}')
         data, columns = read_table(db_details, table_name)
-    
-    for column in columns:
-        print(column)
+        print(build_insert_query(table_name, columns))
+        print(f'loading data for {table_name}')
 
 
 if __name__ == '__main__':
--- scripts/write.py
+++ scripts/write.py
@@ -1,1 +1,21 @@
 from util import get_connection
+
+def build_insert_query(table_name, column_names):
+    column_values = tuple(map(lambda column: column.replace(column, '%s'), column_names))
+    query = (f'''
+        INSERT INTO {table_name} {column_names} VALUES {column_values}
+    ''')
+    return query
+
+def insert_data(connection, cursor, query, data, batch_size = 100):
+    recs = []
+    count = 1
+    for rec in data:
+        recs.append(rec)
+        if count % batch_size == 0:
+            cursor.executemany(query, recs)
+            connection.commit()
+            recs = []
+        count = count + 1
+    cursor.executemany(query, recs)
+    connection.commit()
