082c64d705e928b89084de8eda73f8b16bf24219
HtetAungHlaing24899 <htetaunghlaing240899@gmail.com>
2024-09-10 22:14:51 +0700

    adding lines to create a table in postgres before loading data

--- scripts/app.py
+++ scripts/app.py
@@ -1,9 +1,10 @@
 import sys
-from config import DB_DETAILS
+from config import DB_DETAILS, TYPE_MAPPING
 from util import get_tables
 from util import load_db_details
-from read import read_table
+from read import read_table, read_column_types
 from write import build_insert_query
+from create import create_table
 
 def main():
     #getting arguments from command line and get db_details based on the argument
@@ -14,7 +15,22 @@ def main():
     tables = get_tables('../table_list.txt')
     for table_name in tables['table_name']:
         print(f'reading data for {table_name}')
+
+        #reading data from mysql
         data, columns = read_table(db_details, table_name)
+
+        #reading column types from mysql to create a new table in postgres
+        column_names, column_types = read_column_types(db_details, table_name)
+
+        #replacing column types with postgres types using dict
+        for i in range(len(column_types)):
+            column_types[i] = TYPE_MAPPING[column_types[i]]
+
+        #create table in postgres
+        print(f'creat table {table_name} in postgres server')
+        create_table(db_details, table_name, column_names, column_types)
+        print(f'completed creating table')
+
         print(build_insert_query(table_name, columns))
         print(f'loading data for {table_name}')
 
