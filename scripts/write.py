8156b7e300d1f0d1235fd690fc6c68c88dae1bcd
HtetAungHlaing24899 <htetaunghlaing240899@gmail.com>
2024-09-14 15:55:26 +0700

    modified column names and values string storing format

--- scripts/write.py
+++ scripts/write.py
@@ -1,9 +1,11 @@
 from util import get_connection
 
 def build_insert_query(table_name, column_names):
+    column_names_string = ', '.join(column_names)
     column_values = tuple(map(lambda column: column.replace(column, '%s'), column_names))
+    column_values_string = ', '.join(column_values)
     query = (f'''
-        INSERT INTO {table_name} {column_names} VALUES {column_values}
+        INSERT INTO {table_name} ({column_names_string}) VALUES ({column_values_string})
     ''')
     return query
 
